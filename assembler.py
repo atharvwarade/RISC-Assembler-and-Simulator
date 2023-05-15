a511061101c6d9cf4bc4aa7a5937594dcc67a957
label33 <133559452+label33@users.noreply.github.com>
2023-05-15 23:46:28 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -1,1 +1,11 @@
-
+register_num_list=[0,1,2,3,4,5,6]
+def list_convert(s):
+   k=[]
+   ret=[]
+   k.clear()
+   ret.clear()
+   k=s.split()
+   for i in k:
+       if i.strip()!="":
+           ret.append(i.strip())
+   return ret
