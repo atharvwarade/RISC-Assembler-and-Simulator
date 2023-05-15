645f2600dc9fc6cab9ebecdd39ba2b64fc57d69a
label33 <133559452+label33@users.noreply.github.com>
2023-05-15 23:50:38 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -245,3 +245,71 @@ def error_checker():
     if (flag):
         return 0
     return 1
+disc_isa = {"add" : {"opcode" : "00000", "type" : "a"},
+            "sub" : {"opcode" : "00001", "type" : "a"},
+            "mov1" : {"opcode" : "00010", "type" : "b"},
+            "mov2" : {"opcode" : "00011", "type" : "c"},
+            "ld" : {"opcode" : "00100", "type" : "d"},
+            "st" : {"opcode" : "00101", "type" : "d"},
+            "mul" : {"opcode" : "00110", "type" : "a"},
+            "div" : {"opcode" : "00111", "type" : "c"},
+            "rs" : {"opcode" : "01000", "type" : "b"},
+            "ls" : {"opcode" : "01001", "type" : "b"},
+            "xor" : {"opcode" : "01010", "type" : "a"},
+            "or" : {"opcode" : "01011", "type" : "a"},
+            "and" : {"opcode" : "01100", "type" : "a"},
+            "not" : {"opcode" : "01101", "type" : "c"},
+            "cmp" : {"opcode" : "01110", "type" : "c"},
+            "jmp" : {"opcode" : "01111", "type" : "e"},
+            "jlt" : {"opcode" : "11100", "type" : "e"},
+            "jgt" : {"opcode" : "11101", "type" : "e"},
+            "je" : {"opcode" : "11111", "type" : "e"},
+            "hlt" : {"opcode" : "11010", "type" : "f"}}
+disc_reg = {"R0" : "000","R1" : "001", "R2" : "010" , "R3" : "011" , "R4" : "100" , "R5" : "101" , "R6" :"110", "FLAGS" : "111"}
+reg_values={"R0":0,"R1":0,"R2":0,"R3":0,"R4":0,"R5":0,"R6":0,"FLAGS":"0000000000000000"}
+opcode=[]
+def DecimalToBinary(num) : #This will return string of the binary conversion
+    if num<1 :
+        return ""
+    ans = DecimalToBinary(num//2)
+    return ans+str(num%2)
+opcode_list = []
+unused={"a":2,"b":1,"c":5,"d":1,"e":4,"f":11}
+length_opcode=[4,3,2,1]
+f=open("test01.txt","r")
+k=f.read().splitlines()
+# print(len(k))
+g=open("opcode.txt","w")
+c=0
+variables={}
+labels={}
+for i in range (len(k)):
+    p=list_convert(k[i])
+    if p[0] != "var":
+        c=c+1
+for i in range (len(k)):
+    p=list_convert(k[i])
+    if p[0]=="var":
+        if len(p)==2:
+            if p[0] not in variables:
+                binary=DecimalToBinary(c)
+                binary=(7-len(binary))*"0"+binary
+                variables[p[1]]=binary
+                c=c+1
+c_lab=0
+for i in range(len(k)):
+    p=list_convert(k[i])
+    if p[0]!="var":
+        if p[0][-1]==":":
+            if len(p)==1:
+                if p[0] not in labels:
+                    binary=DecimalToBinary(c_lab+1)
+                    binary=(7-len(binary))*"0"+binary
+                    labels [p[0]]=binary
+            else:
+                if p[0] not in labels:
+                    binary=DecimalToBinary(c_lab)
+                    binary=(7-len(binary))*"0"+binary
+                    labels[p[0][0:-1]]=binary
+        c_lab+=1
+count=0
